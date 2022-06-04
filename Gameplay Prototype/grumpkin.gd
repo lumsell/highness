9ae4cf78b33b6bca979a809069956717c36a6b10
1Jamster1 <jasper@live.com.au>
2022-06-04 20:46:20 +1000

    Basic pathfinding on enemies

--- Gameplay Prototype/grumpkin.gd
+++ Gameplay Prototype/grumpkin.gd
@@ -1,10 +1,31 @@
 extends Node2D
 
+export var speed = 50
+var path = PoolVector2Array() 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if path.size() == 0:
+		pass
+	var move_distance = speed * delta
+	
+	var start_point = position
+	for i in range(path.size()):
+		var distance_to_next_point = start_point.distance_to(path[0])
+		if move_distance <= distance_to_next_point and move_distance >= 0.0:
+			position = start_point.linear_interpolate(path[0], move_distance / distance_to_next_point)
+			break
+		elif move_distance < 0.0:
+			position = path[0]
+		move_distance -- distance_to_next_point
+		start_point = path[0]
+		path.remove(0)
+	
+	
+		
+	
+	
---
+++ Gameplay Prototype/overworld.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+onready var nav2d : Navigation2D = $Navigation2D
+onready var player : Node2D = $tyrannia
+onready var enemy1 : Node2D	 = $grumpkin
+onready var enemy2 : Node2D = $grumpkin2
+onready var debugLine : Line2D = $debugPathLine
+onready var debugLine2 : Line2D = $debugPathLine2
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	enemy1.path = get_path_to_player(player, enemy1)
+	enemy2.path = get_path_to_player(player, enemy2)
+	
+	debugLine.points = enemy1.path
+	debugLine2.points = enemy2.path
+		
+	
+func get_path_to_player(player, enemy):
+	return nav2d.get_simple_path(enemy.position, player.position)
